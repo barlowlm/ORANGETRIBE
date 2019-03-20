9edc1590083d463c969a0f4a227800d0fdb3b9db
Ruth Brennan <ruthbrennan@Ruths-MacBook-Pro.local>
2019-03-20 23:02:47 +0000

    data displayed with markers

--- mapTutorial/App.js
+++ mapTutorial/App.js
@@ -9,7 +9,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, {Callout} from 'react-native-maps';
 import Login from './components/Login'
 
 const instructions = Platform.select({
@@ -27,50 +27,93 @@ export default class App extends Component<Props> {
 
   componentDidMount = () => {
     fetch('http://sloder.netsoc.ie/server.php',{
-      method: 'GET'
+      method: 'POST'
     })
     .then((response) => response.json())
     .then((responseJson) => {
       console.log(responseJson);
       console.log("test");
       this.setState({
-        data: responseJson
+        data: JSON.parse(responseJson)
       })
+      console.log(this.state.data);
+      console.log(this.state.data.features[0].geometry.coordinates[0]);
     })
     .catch((error) => {
       console.error(error);
     });
   }
-  render() {
-    return (
-  /*
-      <View>
-        <Text>
-          {this.state.data.body}
-        </Text>
-      </View> 
-*/
 
-    <MapView
-    style={styles.mapStyle}
-  initialRegion={{
-    latitude: 8.7832,
-    longitude: 34.5085,
-    latitudeDelta: 110,
-    longitudeDelta: 20,
-    }}
-  >
-    <MapView.Marker
-      coordinate = {{
-        latitude: 8.7832,
-        longitude: 34.5085,
-      }}
-    />
-  </MapView>
-  
-    );
+  render() {
+    
+    if (this.state.data=='') {
+      return (
+        <View style={styles.container}>
+          <Text>Loading...</Text>
+        </View> 
+      );
+    } else {
+      return (
+        <MapView
+          style={styles.mapStyle}
+        initialRegion={{
+          latitude: this.state.data.features[0].geometry.coordinates[0][0][1],
+          longitude: this.state.data.features[0].geometry.coordinates[0][0][0],
+          latitudeDelta: 2, //110,
+          longitudeDelta: 2, //20,
+          }}
+        >
+
+          <MapView.Marker
+            coordinate = {{
+              latitude: this.state.data.features[0].geometry.coordinates[0][0][1],
+              longitude: this.state.data.features[0].geometry.coordinates[0][0][0],
+            }}
+          > 
+          </MapView.Marker>
+
+
+
+        </MapView>
+      );
+    }
   }
 }
+
+/*
+          <MapView.Polygon
+            
+            coordinate = {this.state.data.features[0].geometry.coordinates[0]}
+            strokeColor="#F00"
+            fillColor="rgba(255,0,0,0.5)"
+            strokeWidth={1}
+          /> */
+
+/*
+<MapView
+          style={styles.mapStyle}
+        initialRegion={{
+          latitude: 8.7832,
+          longitude: 34.5085,
+          latitudeDelta: 110,
+          longitudeDelta: 20,
+          }}this.state.data.features[0].geometry.coordinates[0]
+        >
+
+        <MapView.Marker
+            coordinate = {{
+              latitude: [0][1],
+              longitude: this.state.data.features[0].geometry.coordinates[0][0][0],
+            }}
+          /> 
+        
+          
+        </MapView> 
+
+  ***************************
+
+
+*/
 
 const styles = StyleSheet.create({
   container: {
